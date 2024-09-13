4eab04dbd76253f5996cfd09749ff6b3c0c591a3
SLipWGH <SLipWMB@yandex.ru>
2024-09-13 15:51:41 +0300

    db tables refactored

--- backend/src/app/tables.py
+++ backend/src/app/tables.py
@@ -1,43 +1,84 @@
-from datetime import datetime
-from uuid import uuid4
-from typing import List
-import enum
+import datetime
+import uuid
+from typing import Annotated, Literal
 
-from sqlalchemy import Enum, String, JSON, Uuid
+
+from sqlalchemy import func, String, JSON, UUID, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
+PreferedLanguage = Literal['Русский', 'English']
 
-class PreferedLanguage(enum.Enum):
-    RUSSIAN = 'ru'
-    ENGLISH = 'en'
+timestamp = Annotated[
+    datetime.datetime,
+    mapped_column(
+        nullable=False, 
+        server_default=func.CURRENT_TIMESTAMP()
+    )
+]
 
+pkuuid = Annotated[
+    uuid.UUID,
+    mapped_column(
+        UUID(), 
+        primary_key=True, 
+        default=uuid.uuid4, 
+        nullable=False
+    )
+]
 
 class Base(DeclarativeBase):
     type_annotation_map = {
         dict[PreferedLanguage, str]: JSON,
-        List[dict[str, datetime]]: JSON,
-        PreferedLanguage: Enum(
-            PreferedLanguage, 
-            native_enum=False
-        )
     }
+
+    def __repr__(self):
+        cols = []
+        for col in self.__table__.columns.keys():
+            cols.append(f"{col}={getattr(self,col)}")
+        return f"<{self.__class__.__name__} {','.join(cols)}"
 
 
 class User(Base):
     __tablename__ = "users_table"
 
-
-    id : Mapped[uuid4] = mapped_column(Uuid(), primary_key=True)
-    username : Mapped[str] = mapped_column(String(64))
-    prefered_language : Mapped[PreferedLanguage]
-    achievements_list: Mapped[List[dict[str, datetime]]]
+    id : Mapped[pkuuid]
+    username : Mapped[str] = mapped_column(String(64), nullable = False, unique=True)
+    prefered_language : Mapped[PreferedLanguage] = mapped_column(nullable=False)
 
 
-class Achievements(Base):
+class Achievement(Base):
     __tablename__ = 'achievements_table'
 
-    name : Mapped[str] = mapped_column(String(256), primary_key=True)
+    name : Mapped[str] = mapped_column(String(128), primary_key=True, nullable=False)
     value : Mapped[int]
-    desctiprion : Mapped[dict[PreferedLanguage, str]]
+    description : Mapped[dict[PreferedLanguage, str]] 
+
+
+class UsersAchievements(Base):
+    __tablename__ = 'users_achievements_table'
+
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey(
+            "users_table.id", 
+            ondelete="CASCADE", 
+            onupdate="CASCADE"
+        ), 
+        nullable=False,
+        primary_key=True
+    )
+    achievement_name: Mapped[str] = mapped_column(
+        String(64), 
+        ForeignKey(
+            "achievements_table.name", 
+            ondelete="CASCADE", 
+            onupdate="CASCADE"
+        ), 
+        nullable=False,
+        primary_key=True
+    )
+    date: Mapped[timestamp]
+
+
+
 
