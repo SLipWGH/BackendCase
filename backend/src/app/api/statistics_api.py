b648cd0613fe0438bd624c6f6d549341f1ec7147
SLipWGH <SLipWMB@yandex.ru>
2024-09-13 15:50:35 +0300

    user and statistics api implemented

--- backend/src/app/api/statistics_api.py
+++ backend/src/app/api/statistics_api.py
@@ -1,4 +1,10 @@
-from fastapi import APIRouter
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database import get_async_session
+from app.models.user_models import ShowUser
+from app.dals.statistics_dal import StatisticsDAL
 
 statistics_router = APIRouter(
     prefix="/statistics",
@@ -8,34 +14,102 @@ statistics_router = APIRouter(
 
 @statistics_router.get("/get-max-achievements-count-user")
 async def get_max_achievements_count_user(
-    session
-):
-    pass
+    session: AsyncSession = Depends(get_async_session)
+)-> ShowUser:
+    return await _get_user_with_max_achievements_count(session)
 
 
 @statistics_router.get("/get-max-achievements-value-user")
 async def get_max_achievements_value_user(
-    session
-):
-    pass
+    session: AsyncSession = Depends(get_async_session)
+)-> ShowUser:
+    return await _get_user_with_max_achievements_value_summ(session)
 
 
 @statistics_router.get("/get-max-difference-achievements-value-users")
 async def get_max_difference_achievements_value_users(
-    session
-):
-    pass
+    session: AsyncSession = Depends(get_async_session)
+)-> tuple[ShowUser, ShowUser]:
+    return await _get_user_with_max_achievements_value_summ(session)
 
 
-@statistics_router.get("/get-min-diggerence-achivements-value-users")
-async def get_min_diggerence_achivements_value_users(
-    session
-):
-    pass
+@statistics_router.get("/get-min-difference-achivements-value-users")
+async def get_min_difference_achivements_value_users(
+    session: AsyncSession = Depends(get_async_session)
+)-> tuple[ShowUser, ShowUser]:
+    return await _get_users_with_min_achievements_value_difference(session)
 
 
 @statistics_router.get("/get-week-achievement-streak-users")
 async def get_week_achievement_streak_users(
-    session
-):
-    pass
+    session: AsyncSession = Depends(get_async_session)
+)-> list[ShowUser]:
+    return await _get_week_achievement_streak_users(session)
+
+
+async def _get_user_with_max_achievements_count(
+    session: AsyncSession
+)-> ShowUser:
+    async with session:
+        async with session.begin():
+            statistics_dal = StatisticsDAL(session)
+            user = await statistics_dal.get_max_achievements_user()
+            return ShowUser(
+                user_id=user.id,
+                username=user.username,
+                prefered_language=user.prefered_language
+            )
+        
+
+async def _get_user_with_max_achievements_value_summ(
+    session: AsyncSession
+)-> ShowUser:
+    async with session:
+        async with session.begin():
+            statistics_dal = StatisticsDAL(session)
+            user = await statistics_dal.get_max_achievements_value_sum_user()
+            return ShowUser(
+                user_id=user.id,
+                username=user.username,
+                prefered_language=user.prefered_language
+            )
+
+
+async def _get_users_with_min_achievements_value_difference(
+    session: AsyncSession
+)-> tuple[ShowUser, ShowUser]:
+    async with session:
+        async with session.begin():
+            statistics_dal = StatisticsDAL(session)
+            user1, user2 = await statistics_dal.users_with_min_achievements_value_difference()
+            return (
+                ShowUser(
+                    user_id=user1.id,
+                    username=user1.username,
+                    prefered_language=user1.prefered_language
+                ),
+                ShowUser(
+                    user_id=user2.id,
+                    username=user2.username,
+                    prefered_language=user2.prefered_language
+                )
+            )
+
+
+async def _get_week_achievement_streak_users(
+    session: AsyncSession
+)-> list[ShowUser]:
+    async with session:
+        async with session.begin():
+            statistics_dal = StatisticsDAL(session)
+            result = await statistics_dal.get_week_achievement_streak_users()
+            users = []
+            for user in result:
+                users.append(ShowUser(
+                    user_id=user.user_id,
+                    username=user.username,
+                    prefered_language=user.prefered_language
+                ))
+
+            return users
+
--- backend/src/app/api/user_api.py
+++ backend/src/app/api/user_api.py
@@ -1,4 +1,12 @@
-from fastapi import APIRouter
+from uuid import UUID
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.dals.user_dal import UserDAL
+from app.models.user_models import UserCreate, ShowUser, AddUserAchievement, UserAchievement, ShowUserAchievements
+from app.database import get_async_session
+from pydantic import UUID4
 
 user_router = APIRouter(
     prefix="/users",
@@ -6,34 +14,116 @@ user_router = APIRouter(
 )
 
 
-@user_router.post("/add-user")
-async def add_new_user(
-    user,
-    session
-)-> None:
-    pass
+@user_router.post("/", response_model=ShowUser)
+async def create_user(
+    body: UserCreate,
+    session: AsyncSession = Depends(get_async_session)
+)-> ShowUser:
+    return await _create_new_user(body, session)
 
 
-@user_router.post("/add-user-achievement")
+@user_router.put("/achievements")
 async def add_user_achievement(
-    user,
-    achievement,
-    session,
+    body: AddUserAchievement,
+    session: AsyncSession = Depends(get_async_session),
 )-> None:
-    pass
+    return await _add_user_achievement(body, session)
 
 
-@user_router.get("/get-user-data")
+@user_router.get("/{user_id}")
 async def get_user_data(
-    user,
-    session
+    user_id : UUID,
+    session: AsyncSession = Depends(get_async_session)
 ):
-    pass
+    return await _get_user_data(user_id, session)
 
 
-@user_router.get("/get-user-achievements-data")
-async def get_user_achievements_data(
-    user,
-    session
+@user_router.get("/achievements/{user_id}")
+async def get_user_achievements(
+    user_id : str,
+    session: AsyncSession = Depends(get_async_session)
 ):
-    pass
+    user_id = await validate_uuid(user_id)
+    return await _get_user_achievements(user_id, session)
+
+
+async def _create_new_user(
+    body: UserCreate,
+    session: AsyncSession
+)-> ShowUser:
+    async with session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            user = await user_dal.create_user(
+                username=body.username,
+                prefered_language=body.prefered_language.value
+            )
+            return ShowUser(
+                user_id=user.id,
+                username=user.username,
+                prefered_language=user.prefered_language
+            )
+        
+
+async def _add_user_achievement(
+    body: AddUserAchievement,
+    session: AsyncSession
+)-> None:
+    async with session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            await user_dal.add_user_achievement_(
+                user_id=body.user_id,
+                achievement_name=body.achievement_name
+            )
+
+
+async def _get_user_data(
+    user_id: UUID,
+    session: AsyncSession
+)-> ShowUser:
+    async with session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            user = await user_dal.get_user_by_id(user_id)
+            return ShowUser(
+                user_id=user_id,
+                username=user.username,
+                prefered_language=user.prefered_language        
+            )
+            
+
+async def _get_user_achievements(
+    user_id: UUID,
+    session: AsyncSession
+)-> ShowUserAchievements:
+    async with session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            result, lang = await user_dal.get_user_achievements_list(user_id) 
+            user_achievements = []
+            for user_achievement in result:
+                date, achievement = user_achievement
+                user_achievements.append(
+                    UserAchievement(
+                        achievement_name=achievement.name,
+                        achievement_value=achievement.value,
+                        achievement_description=achievement.description[lang],
+                        achievement_date=date
+                    )
+                )
+            return ShowUserAchievements(user_achievements=user_achievements)
+
+
+async def validate_uuid(
+    uuid: str
+)-> UUID:
+    try:
+        id_validated =  UUID(uuid)
+    except ValueError as e:
+            return JSONResponse(
+            status_code=400,
+            content={"message": f"Oops! your streem id is not a valid UUID..."},
+        )
+    return id_validated
+    
